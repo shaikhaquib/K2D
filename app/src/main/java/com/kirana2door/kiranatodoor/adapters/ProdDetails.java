b7051d99f85a77cfd4b2b556b5d8e3791a1ae23c
Lenovo <pbhushan246@gmail.com>
2019-02-21 09:20:43 +0530

    bhushan files commits on 21-02-2019

---
+++ app/src/main/java/com/kirana2door/kiranatodoor/adapters/ProdDetails.java
@@ -0,0 +1,40 @@
+package com.kirana2door.kiranatodoor.adapters;
+
+import java.util.List;
+
+import com.google.gson.annotations.SerializedName;
+
+
+public class ProdDetails{
+
+	@SerializedName("product_images")
+	private List<ProductImagesItem> productImages;
+
+	@SerializedName("ProductDetails")
+	private List<ProductDetailsItem> productDetails;
+
+	public void setProductImages(List<ProductImagesItem> productImages){
+		this.productImages = productImages;
+	}
+
+	public List<ProductImagesItem> getProductImages(){
+		return productImages;
+	}
+
+	public void setProductDetails(List<ProductDetailsItem> productDetails){
+		this.productDetails = productDetails;
+	}
+
+	public List<ProductDetailsItem> getProductDetails(){
+		return productDetails;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"ProdDetails{" + 
+			"product_images = '" + productImages + '\'' + 
+			",productDetails = '" + productDetails + '\'' + 
+			"}";
+		}
+}
---
+++ app/src/main/java/com/kirana2door/kiranatodoor/adapters/ProductImagesItem.java
@@ -0,0 +1,51 @@
+package com.kirana2door.kiranatodoor.adapters;
+
+
+import com.google.gson.annotations.SerializedName;
+
+
+public class ProductImagesItem{
+
+	@SerializedName("product_id")
+	private String productId;
+
+	@SerializedName("product_img_id")
+	private String productImgId;
+
+	@SerializedName("product_img")
+	private String productImg;
+
+	public void setProductId(String productId){
+		this.productId = productId;
+	}
+
+	public String getProductId(){
+		return productId;
+	}
+
+	public void setProductImgId(String productImgId){
+		this.productImgId = productImgId;
+	}
+
+	public String getProductImgId(){
+		return productImgId;
+	}
+
+	public void setProductImg(String productImg){
+		this.productImg = productImg;
+	}
+
+	public String getProductImg(){
+		return productImg;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"ProductImagesItem{" + 
+			"product_id = '" + productId + '\'' + 
+			",product_img_id = '" + productImgId + '\'' + 
+			",product_img = '" + productImg + '\'' + 
+			"}";
+		}
+}
