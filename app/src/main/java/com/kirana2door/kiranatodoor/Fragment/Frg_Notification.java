e9af3b68453c006479179f529f7e062161492457
Lenovo <pbhushan246@gmail.com>
2019-02-22 09:36:50 +0530

    bhushan files commits on 22-02-2019

--- app/src/main/java/com/kirana2door/kiranatodoor/Fragment/Frg_Notification.java
+++ app/src/main/java/com/kirana2door/kiranatodoor/Fragment/Frg_Notification.java
@@ -12,31 +12,46 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.bumptech.glide.Glide;
+import com.google.gson.Gson;
+import com.kirana2door.kiranatodoor.Global;
 import com.kirana2door.kiranatodoor.R;
+import com.kirana2door.kiranatodoor.ViewDialog;
+import com.kirana2door.kiranatodoor.adapters.OrderHistoryList;
+import com.kirana2door.kiranatodoor.adapters.OrderHistoryListItem;
+import com.kirana2door.kiranatodoor.api.AppController;
+import com.kirana2door.kiranatodoor.api.RetrofitClient;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 import params.com.stepview.StatusView;
 import params.com.stepview.StatusViewScroller;
 
 public class Frg_Notification extends Fragment {
 
-    RecyclerView rvHist;
-     String[] prd_img = {
-        "https://www.bigbasket.com/media/uploads/p/s/40112512_2-bb-royal-medjool-dates.jpg",
-        "https://www.bigbasket.com/media/uploads/p/s/40072455_5-bb-royal-organic-kabuli-chanachanna.jpg",
-        "https://www.bigbasket.com/media/uploads/p/s/40112523_2-bb-royal-dried-fruit-blueberries.jpg"
-        },
-        prd_names = {
-                "STOCK-LIST NAME 1",
-                "STOCK-LIST NAME 2",
-                "STOCK-LIST NAME 3"
-        },prd_qnt = {"9","15","6"},prd_status = {"3","1","4"};
 
+    List<OrderHistoryListItem> ordlist = new ArrayList<>();
+    ViewDialog progressDialog;
+    RecyclerView rvHist;
+    LinkedHashMap map;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view =  inflater.inflate(R.layout.frg_notification, null);
-
+        progressDialog=new ViewDialog(getActivity());
+        map = new LinkedHashMap<>();
+        map.put("Received",1);
+        map.put("Packed",2);
+        map.put("Shipped",3);
+        map.put("Delivered",4);
         rvHist = view.findViewById(R.id.rvrHist);
         rvHist.setLayoutManager(new LinearLayoutManager(getActivity()));
         rvHist.setAdapter(new RecyclerView.Adapter() {
@@ -51,12 +66,19 @@ public class Frg_Notification extends Fragment {
             @Override
             public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
                 final Holder myHolder= (Holder) viewHolder;
-                Glide.with(getActivity()).load(prd_img[i]).into(myHolder.img);
+                final OrderHistoryListItem model = ordlist.get(i);
+                Glide.with(getActivity()).load(model.getLogoImg()).into(myHolder.img);
+                myHolder.prdname.setText(model.getShopName());
+                myHolder.prdqnt.setText(model.getOrderId());
+                myHolder.indicator.setTag(i);
+                StatusView Status = myHolder.indicator.getStatusView();
+                Status.setCurrentCount(Integer.parseInt(String.valueOf(map.get(model.getOrderStatus()))));
+                /*Glide.with(getActivity()).load(prd_img[i]).into(myHolder.img);
                 myHolder.prdname.setText(prd_names[i]);
                 myHolder.prdqnt.setText("Total Item Ordered : "+prd_qnt[i]);
                 myHolder.indicator.setTag(i);
                 StatusView Status = myHolder.indicator.getStatusView();
-                Status.setCurrentCount(Integer.parseInt(prd_status[i]));
+                Status.setCurrentCount(Integer.parseInt(prd_status[i]));*/
 
 
 
@@ -64,7 +86,7 @@ public class Frg_Notification extends Fragment {
 
             @Override
             public int getItemCount() {
-                return prd_img.length;
+                return ordlist.size();
             }
             class Holder extends RecyclerView.ViewHolder {
                 ImageView img;
@@ -84,7 +106,37 @@ public class Frg_Notification extends Fragment {
             } }
         );
 
-
+        getData();
         return view;
+    }
+
+    private void getData() {
+        //data variables call
+        progressDialog.show();
+        StringRequest stringRequest = new StringRequest(StringRequest.Method.POST, RetrofitClient.BASE_URL+"orderhistory", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                progressDialog.dismiss();
+                Gson gson = new Gson();
+                OrderHistoryList res = gson.fromJson(response, OrderHistoryList.class);
+                ordlist = res.getOrderHistoryList();
+                rvHist.getAdapter().notifyDataSetChanged();
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                progressDialog.dismiss();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map <String,String> param = new HashMap<String,String>();
+                param.put("custid", Global.customer_id);
+                return param;
+            }
+        };
+        AppController.getInstance().addToRequestQueue(stringRequest);
     }
 }
---
+++ app/src/main/java/com/kirana2door/kiranatodoor/adapters/OrderHistoryListItem.java
@@ -0,0 +1,494 @@
+package com.kirana2door.kiranatodoor.adapters;
+
+
+import com.google.gson.annotations.SerializedName;
+
+public class OrderHistoryListItem{
+
+	@SerializedName("shipping_charges")
+	private String shippingCharges;
+
+	@SerializedName("closingtime")
+	private String closingtime;
+
+	@SerializedName("mobile_no")
+	private String mobileNo;
+
+	@SerializedName("gst")
+	private String gst;
+
+	@SerializedName("total_ammount")
+	private String totalAmmount;
+
+	@SerializedName("order_status")
+	private String orderStatus;
+
+	@SerializedName("oampm")
+	private String oampm;
+
+	@SerializedName("status_change_time")
+	private String statusChangeTime;
+
+	@SerializedName("delivery_type")
+	private String deliveryType;
+
+	@SerializedName("landline_no")
+	private String landlineNo;
+
+	@SerializedName("id")
+	private String id;
+
+	@SerializedName("first_name")
+	private String firstName;
+
+	@SerializedName("email")
+	private String email;
+
+	@SerializedName("pincode")
+	private String pincode;
+
+	@SerializedName("invoice_path")
+	private String invoicePath;
+
+	@SerializedName("address")
+	private String address;
+
+	@SerializedName("last_name")
+	private String lastName;
+
+	@SerializedName("delivery_state")
+	private String deliveryState;
+
+	@SerializedName("shop_name")
+	private String shopName;
+
+	@SerializedName("campm")
+	private String campm;
+
+	@SerializedName("final_ammount")
+	private String finalAmmount;
+
+	@SerializedName("created_by")
+	private String createdBy;
+
+	@SerializedName("shop_id")
+	private String shopId;
+
+	@SerializedName("order_date")
+	private String orderDate;
+
+	@SerializedName("delivery_add1")
+	private String deliveryAdd1;
+
+	@SerializedName("logo_img")
+	private String logoImg;
+
+	@SerializedName("latlang")
+	private String latlang;
+
+	@SerializedName("gst_ammount")
+	private String gstAmmount;
+
+	@SerializedName("payment_type")
+	private String paymentType;
+
+	@SerializedName("openingtime")
+	private String openingtime;
+
+	@SerializedName("delivery_add2")
+	private String deliveryAdd2;
+
+	@SerializedName("delivery_add3")
+	private String deliveryAdd3;
+
+	@SerializedName("updated_by")
+	private String updatedBy;
+
+	@SerializedName("created_date")
+	private String createdDate;
+
+	@SerializedName("updated_date")
+	private String updatedDate;
+
+	@SerializedName("customer_id")
+	private String customerId;
+
+	@SerializedName("order_id")
+	private String orderId;
+
+	@SerializedName("delivery_city")
+	private String deliveryCity;
+
+	@SerializedName("city_id")
+	private String cityId;
+
+	@SerializedName("status")
+	private String status;
+
+	public void setShippingCharges(String shippingCharges){
+		this.shippingCharges = shippingCharges;
+	}
+
+	public String getShippingCharges(){
+		return shippingCharges;
+	}
+
+	public void setClosingtime(String closingtime){
+		this.closingtime = closingtime;
+	}
+
+	public String getClosingtime(){
+		return closingtime;
+	}
+
+	public void setMobileNo(String mobileNo){
+		this.mobileNo = mobileNo;
+	}
+
+	public String getMobileNo(){
+		return mobileNo;
+	}
+
+	public void setGst(String gst){
+		this.gst = gst;
+	}
+
+	public String getGst(){
+		return gst;
+	}
+
+	public void setTotalAmmount(String totalAmmount){
+		this.totalAmmount = totalAmmount;
+	}
+
+	public String getTotalAmmount(){
+		return totalAmmount;
+	}
+
+	public void setOrderStatus(String orderStatus){
+		this.orderStatus = orderStatus;
+	}
+
+	public String getOrderStatus(){
+		return orderStatus;
+	}
+
+	public void setOampm(String oampm){
+		this.oampm = oampm;
+	}
+
+	public String getOampm(){
+		return oampm;
+	}
+
+	public void setStatusChangeTime(String statusChangeTime){
+		this.statusChangeTime = statusChangeTime;
+	}
+
+	public String getStatusChangeTime(){
+		return statusChangeTime;
+	}
+
+	public void setDeliveryType(String deliveryType){
+		this.deliveryType = deliveryType;
+	}
+
+	public String getDeliveryType(){
+		return deliveryType;
+	}
+
+	public void setLandlineNo(String landlineNo){
+		this.landlineNo = landlineNo;
+	}
+
+	public String getLandlineNo(){
+		return landlineNo;
+	}
+
+	public void setId(String id){
+		this.id = id;
+	}
+
+	public String getId(){
+		return id;
+	}
+
+	public void setFirstName(String firstName){
+		this.firstName = firstName;
+	}
+
+	public String getFirstName(){
+		return firstName;
+	}
+
+	public void setEmail(String email){
+		this.email = email;
+	}
+
+	public String getEmail(){
+		return email;
+	}
+
+	public void setPincode(String pincode){
+		this.pincode = pincode;
+	}
+
+	public String getPincode(){
+		return pincode;
+	}
+
+	public void setInvoicePath(String invoicePath){
+		this.invoicePath = invoicePath;
+	}
+
+	public String getInvoicePath(){
+		return invoicePath;
+	}
+
+	public void setAddress(String address){
+		this.address = address;
+	}
+
+	public String getAddress(){
+		return address;
+	}
+
+	public void setLastName(String lastName){
+		this.lastName = lastName;
+	}
+
+	public String getLastName(){
+		return lastName;
+	}
+
+	public void setDeliveryState(String deliveryState){
+		this.deliveryState = deliveryState;
+	}
+
+	public String getDeliveryState(){
+		return deliveryState;
+	}
+
+	public void setShopName(String shopName){
+		this.shopName = shopName;
+	}
+
+	public String getShopName(){
+		return shopName;
+	}
+
+	public void setCampm(String campm){
+		this.campm = campm;
+	}
+
+	public String getCampm(){
+		return campm;
+	}
+
+	public void setFinalAmmount(String finalAmmount){
+		this.finalAmmount = finalAmmount;
+	}
+
+	public String getFinalAmmount(){
+		return finalAmmount;
+	}
+
+	public void setCreatedBy(String createdBy){
+		this.createdBy = createdBy;
+	}
+
+	public String getCreatedBy(){
+		return createdBy;
+	}
+
+	public void setShopId(String shopId){
+		this.shopId = shopId;
+	}
+
+	public String getShopId(){
+		return shopId;
+	}
+
+	public void setOrderDate(String orderDate){
+		this.orderDate = orderDate;
+	}
+
+	public String getOrderDate(){
+		return orderDate;
+	}
+
+	public void setDeliveryAdd1(String deliveryAdd1){
+		this.deliveryAdd1 = deliveryAdd1;
+	}
+
+	public String getDeliveryAdd1(){
+		return deliveryAdd1;
+	}
+
+	public void setLogoImg(String logoImg){
+		this.logoImg = logoImg;
+	}
+
+	public String getLogoImg(){
+		return logoImg;
+	}
+
+	public void setLatlang(String latlang){
+		this.latlang = latlang;
+	}
+
+	public String getLatlang(){
+		return latlang;
+	}
+
+	public void setGstAmmount(String gstAmmount){
+		this.gstAmmount = gstAmmount;
+	}
+
+	public String getGstAmmount(){
+		return gstAmmount;
+	}
+
+	public void setPaymentType(String paymentType){
+		this.paymentType = paymentType;
+	}
+
+	public String getPaymentType(){
+		return paymentType;
+	}
+
+	public void setOpeningtime(String openingtime){
+		this.openingtime = openingtime;
+	}
+
+	public String getOpeningtime(){
+		return openingtime;
+	}
+
+	public void setDeliveryAdd2(String deliveryAdd2){
+		this.deliveryAdd2 = deliveryAdd2;
+	}
+
+	public String getDeliveryAdd2(){
+		return deliveryAdd2;
+	}
+
+	public void setDeliveryAdd3(String deliveryAdd3){
+		this.deliveryAdd3 = deliveryAdd3;
+	}
+
+	public String getDeliveryAdd3(){
+		return deliveryAdd3;
+	}
+
+	public void setUpdatedBy(String updatedBy){
+		this.updatedBy = updatedBy;
+	}
+
+	public String getUpdatedBy(){
+		return updatedBy;
+	}
+
+	public void setCreatedDate(String createdDate){
+		this.createdDate = createdDate;
+	}
+
+	public String getCreatedDate(){
+		return createdDate;
+	}
+
+	public void setUpdatedDate(String updatedDate){
+		this.updatedDate = updatedDate;
+	}
+
+	public String getUpdatedDate(){
+		return updatedDate;
+	}
+
+	public void setCustomerId(String customerId){
+		this.customerId = customerId;
+	}
+
+	public String getCustomerId(){
+		return customerId;
+	}
+
+	public void setOrderId(String orderId){
+		this.orderId = orderId;
+	}
+
+	public String getOrderId(){
+		return orderId;
+	}
+
+	public void setDeliveryCity(String deliveryCity){
+		this.deliveryCity = deliveryCity;
+	}
+
+	public String getDeliveryCity(){
+		return deliveryCity;
+	}
+
+	public void setCityId(String cityId){
+		this.cityId = cityId;
+	}
+
+	public String getCityId(){
+		return cityId;
+	}
+
+	public void setStatus(String status){
+		this.status = status;
+	}
+
+	public String getStatus(){
+		return status;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"OrderHistoryListItem{" + 
+			"shipping_charges = '" + shippingCharges + '\'' + 
+			",closingtime = '" + closingtime + '\'' + 
+			",mobile_no = '" + mobileNo + '\'' + 
+			",gst = '" + gst + '\'' + 
+			",total_ammount = '" + totalAmmount + '\'' + 
+			",order_status = '" + orderStatus + '\'' + 
+			",oampm = '" + oampm + '\'' + 
+			",status_change_time = '" + statusChangeTime + '\'' + 
+			",delivery_type = '" + deliveryType + '\'' + 
+			",landline_no = '" + landlineNo + '\'' + 
+			",id = '" + id + '\'' + 
+			",first_name = '" + firstName + '\'' + 
+			",email = '" + email + '\'' + 
+			",pincode = '" + pincode + '\'' + 
+			",invoice_path = '" + invoicePath + '\'' + 
+			",address = '" + address + '\'' + 
+			",last_name = '" + lastName + '\'' + 
+			",delivery_state = '" + deliveryState + '\'' + 
+			",shop_name = '" + shopName + '\'' + 
+			",campm = '" + campm + '\'' + 
+			",final_ammount = '" + finalAmmount + '\'' + 
+			",created_by = '" + createdBy + '\'' + 
+			",shop_id = '" + shopId + '\'' + 
+			",order_date = '" + orderDate + '\'' + 
+			",delivery_add1 = '" + deliveryAdd1 + '\'' + 
+			",logo_img = '" + logoImg + '\'' + 
+			",latlang = '" + latlang + '\'' + 
+			",gst_ammount = '" + gstAmmount + '\'' + 
+			",payment_type = '" + paymentType + '\'' + 
+			",openingtime = '" + openingtime + '\'' + 
+			",delivery_add2 = '" + deliveryAdd2 + '\'' + 
+			",delivery_add3 = '" + deliveryAdd3 + '\'' + 
+			",updated_by = '" + updatedBy + '\'' + 
+			",created_date = '" + createdDate + '\'' + 
+			",updated_date = '" + updatedDate + '\'' + 
+			",customer_id = '" + customerId + '\'' + 
+			",order_id = '" + orderId + '\'' + 
+			",delivery_city = '" + deliveryCity + '\'' + 
+			",city_id = '" + cityId + '\'' + 
+			",status = '" + status + '\'' + 
+			"}";
+		}
+}
