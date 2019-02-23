9c7aa9450185908b02287aca6dbc77ec97613598
Shaikh Aquib <shaikhaquib119@gmail.com>
2019-02-23 14:59:18 +0530

    Added some file----------

--- app/src/main/java/com/kirana2door/kiranatodoor/Fragment/Frg_Menu.java
+++ app/src/main/java/com/kirana2door/kiranatodoor/Fragment/Frg_Menu.java
@@ -1,5 +1,6 @@
 package com.kirana2door.kiranatodoor.Fragment;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -8,20 +9,54 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.amulyakhare.textdrawable.TextDrawable;
 import com.kirana2door.kiranatodoor.R;
+import com.kirana2door.kiranatodoor.activities.ChangePassword;
 
 public class Frg_Menu extends Fragment {
+
+    TextView mnAccount , mnChangePassword;
+    LinearLayout mnLinearlayout;
+    View mnAccountView;
+    Boolean mnAccountLytState = false;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.frg_menu, null);
 
+        mnAccount = view.findViewById(R.id.mnAccount);
+        mnChangePassword = view.findViewById(R.id.mnChangePassword);
+        mnLinearlayout = view.findViewById(R.id.mnAccountLayout);
+        mnAccountView =view.findViewById(R.id.mnAccountView);
+
+        mnChangePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), ChangePassword.class));
+            }
+        });
 
         ImageView image = view.findViewById(R.id.MnDP);
         TextDrawable drawable = TextDrawable.builder()
                 .buildRound("A", Color.GRAY);
+
+        mnAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!mnAccountLytState){
+                    mnAccountLytState =  true;
+                    mnAccountView.setVisibility(View.GONE);
+                    mnLinearlayout.setVisibility(View.VISIBLE);
+                }else {
+                    mnAccountLytState =  false;
+                    mnAccountView.setVisibility(View.VISIBLE);
+                    mnLinearlayout.setVisibility(View.GONE);
+                }
+            }
+        });
 
         image.setImageDrawable(drawable);
 
--- app/src/main/java/com/kirana2door/kiranatodoor/activities/UpdateContact.java
+++ app/src/main/java/com/kirana2door/kiranatodoor/activities/UpdateContact.java
@@ -5,12 +5,11 @@ import android.os.Bundle;
 
 import com.kirana2door.kiranatodoor.R;
 
-public class ChangePassword extends AppCompatActivity {
+public class UpdateContact extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_change_password);
-        getSupportActionBar().hide();
+        setContentView(R.layout.activity_update_contact);
     }
 }
